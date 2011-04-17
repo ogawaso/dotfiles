14ba93511b3f1b53699add50cc0449e1ac042a3d
ogawaso <ogawaso@gmail.com>
2011-04-17 20:29:54 +0900

    fix setting.rb

--- setting.rb
+++ setting.rb
@@ -2,11 +2,9 @@ require 'fileutils'
 
 
 if __FILE__ == $0
-  home_dir = FileUtils.pwd
+  home_dir = ENV['HOME']
   p home_dir
-  prev_dir = home_dir.gsub("/config", "")
-  p prev_dir
-  FileUtils.mkdir_p("#{prev_dir}/.vim/autoload")
-  FileUtils.symlink("#{home_dir}/.vim/bundle/vim-pathogen/autoload/pathogen.vim", "#{prev_dir}/.vim/autoload/pathogen.vim")
-  FileUtils.symlink("#{home_dir}/.vim/bundle", "#{prev_dir}/.vim/bundle")
+  FileUtils.mkdir_p("#{home_dir}/.vim/autoload")
+  FileUtils.symlink("#{home_dir}/config/.vim/bundle/vim-pathogen/autoload/pathogen.vim", "#{home_dir}/.vim/autoload/pathogen.vim")
+  FileUtils.symlink("#{home_dir}/config/.vim/bundle", "#{home_dir}/.vim/bundle")
 end
