64e809ae73e5740038abaf0ff6f40d53688c7466
ogawaso <ogawaso@gmail.com>
2012-01-04 20:22:06 +0900

    update vundle.git

--- setup.rb
+++ setup.rb
@@ -18,6 +18,8 @@ class Setup
       FileUtils.cp(File.join(current_dir, ".zshrc"), home_dir)
       FileUtils.cp(File.join(current_dir, ".gitconfig"), home_dir)
       FileUtils.cp(File.join(current_dir, ".gemrc"), home_dir)
+      `git submodule init`
+      `git submodule update`
 
     end
   end
