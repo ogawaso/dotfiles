950dbdd09528d9eb136c41f3066d0fe0b086413a
Soichiro Ogawa <ogawaso@gmail.com>
2014-11-21 22:47:31 +0000

    add dev base sh, mv nokogiri install sh

---
+++ script/ubuntu/dev_base.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-get install build-essential -y
+sudo apt-get install curl -y
+sudo apt-get install sysstat -y
+sudo apt-get install ssh -y
+sudo apt-get install screen -y
+sudo apt-get install zsh -y
+
+sudo apt-get install git -y
+sudo apt-get install ctags -y
+sudo apt-get install tig -y
+
