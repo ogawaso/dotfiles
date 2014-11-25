f1835cb430faba9cf8aa95cc3cba8b4281a49ca6
Soichiro Ogawa <ogawaso@gmail.com>
2014-11-25 20:55:33 +0900

    add uninstall homebrew

---
+++ script/osx/uninstall_homebrew.sh
@@ -0,0 +1,8 @@
+cd `brew --prefix`
+rm -rf Cellar
+brew prune
+rm -rf .git
+rm -rf ~/Library/Caches/Homebrew 
+rm -rf ~/Library/Logs/Homebrew
+rm -rf /Library/Caches/Homebrew
+rm -rf Library/Homebrew/
